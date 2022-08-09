6d82bcfe905a9a8ef13fb2391fe5e619d78ecaa3
trhieu310 <trunghieu301999@gmail.com>
2022-08-09 13:38:06 +0700

    update: path dataset

--- configs/_base_/datasets/doc_instance.py
+++ configs/_base_/datasets/doc_instance.py
@@ -1,6 +1,7 @@
 # dataset settings
 dataset_type = 'DocslayoutDataset'
-data_root = '../../../../../../../mnt/datadrive2/quangdq/v2.7.2/'
+# data_root = '../../../../../../../mnt/datadrive2/quangdq/v2.7.2/'
+data_root = '../../input/cascade-mask-rcnn-data/'
 img_norm_cfg = dict(
     mean=[236.56476823, 236.86395663, 237.62402599], std=[29.52565967, 29.79392858, 29.54714306], to_rgb=True)
 train_pipeline = [
@@ -33,17 +34,17 @@ data = dict(
     workers_per_gpu=4,
     train=dict(
         type=dataset_type,
-        ann_file=data_root + 'anns/whole_training_2.7.2.2.json',
-        img_prefix=data_root,
+        ann_file=data_root + 'whole_training_2.7.2.2.json',
+        img_prefix=data_root + 'doc_cascade/doc_data',
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
-        ann_file=data_root + 'anns/whole_testing_2.7.2.json',
-        img_prefix=data_root,
+        ann_file=data_root + 'testing.json',
+        img_prefix=data_root + 'doc_cascade/doc_data',
         pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
-        ann_file=data_root + 'anns/whole_testing_2.7.2.json',
-        img_prefix=data_root,
+        ann_file=data_root + 'testing.json',
+        img_prefix=data_root + 'doc_cascade/doc_data',
         pipeline=test_pipeline))
 evaluation = dict(metric=['bbox', 'segm'])
