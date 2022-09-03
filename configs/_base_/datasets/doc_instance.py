7e047eab3065b27d427c61492dd1a36f68b1c08a
trhieu310 <58017675+trhieu310@users.noreply.github.com>
2022-09-03 14:21:07 +0700

    worker = 0

--- configs/_base_/datasets/doc_instance.py
+++ configs/_base_/datasets/doc_instance.py
@@ -32,8 +32,8 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=2,
-    workers_per_gpu=1,
+    samples_per_gpu=3,
+    workers_per_gpu=0,
     train=dict(
         type=dataset_type,
         classes=classes,
