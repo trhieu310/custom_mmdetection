896b0f4f8bc7ae93d1cde21699d8c9db057721fb
trhieu310 <trunghieu301999@gmail.com>
2022-09-04 12:05:25 +0700

    update base runner

--- configs/_base_/schedules/schedule_1x.py
+++ configs/_base_/schedules/schedule_1x.py
@@ -8,4 +8,5 @@ lr_config = dict(
     warmup_iters=500,
     warmup_ratio=0.001,
     step=[8, 11])
-runner = dict(type='EpochBasedRunner', max_epochs=12)
+# runner = dict(type='EpochBasedRunner', max_epochs=12)
+runner = dict(type='EpochBasedRunner', max_iters=100000) 
