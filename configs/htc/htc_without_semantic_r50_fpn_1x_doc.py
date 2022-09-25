e967f7ea3b0afe781b0bc950ea6de14abb2307b0
hieunt <nthieu.17it1@vku.udn.vn>
2022-09-25 11:06:57 +0700

    unuse semantic for HTCHTC

--- configs/htc/htc_without_semantic_r50_fpn_1x_doc.py
+++ configs/htc/htc_without_semantic_r50_fpn_1x_doc.py
@@ -136,23 +136,23 @@ model = dict(
                 num_classes=3,
                 loss_mask=dict(
                     type='CrossEntropyLoss', use_mask=True, loss_weight=1.0))
-        ],
-        semantic_roi_extractor=dict(
-            type='SingleRoIExtractor',
-            roi_layer=dict(type='RoIAlign', output_size=14, sampling_ratio=0),
-            out_channels=256,
-            featmap_strides=[8]),
-        semantic_head=dict(
-            type='FusedSemanticHead',
-            num_ins=5,
-            fusion_level=1,
-            num_convs=4,
-            in_channels=256,
-            conv_out_channels=256,
-            # num_classes=183, ###Hieunt change num 
-            num_classes=4,
-            loss_seg=dict(
-                type='CrossEntropyLoss', ignore_index=255, loss_weight=0.2))),
+        ]),
+        # semantic_roi_extractor=dict(
+        #     type='SingleRoIExtractor',
+        #     roi_layer=dict(type='RoIAlign', output_size=14, sampling_ratio=0),
+        #     out_channels=256,
+        #     featmap_strides=[8]),
+        # semantic_head=dict(
+        #     type='FusedSemanticHead',
+        #     num_ins=5,
+        #     fusion_level=1,
+        #     num_convs=4,
+        #     in_channels=256,
+        #     conv_out_channels=256,
+        #     # num_classes=183, ###Hieunt change num 
+        #     num_classes=4,
+        #     loss_seg=dict(
+        #         type='CrossEntropyLoss', ignore_index=255, loss_weight=0.2))),
     # model training and testing settings
     train_cfg=dict(
         rpn=dict(
