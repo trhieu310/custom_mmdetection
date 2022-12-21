6d0adbceb6bf6cbc943d1f7602009103b4f68d6d
hieunt <nthieu.17it1@vku.udn.vn>
2022-12-21 23:23:56 +0700

    nb_epoch = 100

--- configs/_base_/schedules/schedule_1x.py
+++ configs/_base_/schedules/schedule_1x.py
@@ -8,5 +8,5 @@ lr_config = dict(
     warmup_iters=500,
     warmup_ratio=0.001,
     step=[8, 11])
-runner = dict(type='EpochBasedRunner', max_epochs=30)
+runner = dict(type='EpochBasedRunner', max_epochs=100)
 # runner = dict(type='IterBasedRunner', max_iters=150000)
