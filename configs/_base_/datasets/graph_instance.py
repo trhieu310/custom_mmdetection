6c3d3bca3d66a337bd761183f4d9889278361490
hieunt <nthieu.17it1@vku.udn.vn>
2022-12-22 01:57:28 +0700

    unuse real data

--- configs/_base_/datasets/graph_instance.py
+++ configs/_base_/datasets/graph_instance.py
@@ -40,9 +40,9 @@ data = dict(
         ann_file=[data_root + 'anns/ann_part_29_graph.json',
                   data_root + 'anns/ann_part_24_graph.json',
                 #   data_root + 'anns/ann_part_25_graph.json', 
-                  data_root + 'anns/ann_part_26_graph.json',
-                  data_root + 'anns/illustration.json',
-                  data_root + 'anns/table.json'],
+                  data_root + 'anns/ann_part_26_graph.json']
+                #   data_root + 'anns/illustration.json',
+                #   data_root + 'anns/table.json'],
         img_prefix=data_root + 'images/',
         pipeline=train_pipeline),
     val=dict(
